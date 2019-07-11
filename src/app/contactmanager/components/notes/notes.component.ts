1ed26c0e420271490fc5a53e76dae510b4090c4d
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-07-11 16:38:36 +0530

    paginator

--- src/app/contactmanager/components/notes/notes.component.ts
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -1,6 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {Note} from '../../models/note';
-import {MatTableDataSource} from '@angular/material';
+import {MatPaginator, MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-notes',
@@ -12,11 +12,18 @@ export class NotesComponent implements OnInit {
   @Input() notes: Note[];
   displayedColumns = ['id', 'title', 'date'];
   dataSource: MatTableDataSource<Note>;
+  // @ts-ignore
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor() { }
 
   ngOnInit() {
     this.dataSource = new MatTableDataSource<Note>(this.notes);
+  }
+
+  // tslint:disable-next-line:use-lifecycle-interface
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
