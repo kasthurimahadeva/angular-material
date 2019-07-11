ba3b943093ac28c8d1208f16c56f3e252868fac9
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-07-11 16:07:17 +0530

    notes component

---
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Note} from '../../models/note';
+
+@Component({
+  selector: 'app-notes',
+  templateUrl: './notes.component.html',
+  styleUrls: ['./notes.component.scss']
+})
+export class NotesComponent implements OnInit {
+
+  @Input() notes: Note[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
