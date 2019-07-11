9bf48c45c2389445d8c3ebca47cda3954183cf80
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-07-11 17:47:14 +0530

    mat-form-field

--- src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
+++ src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
@@ -12,4 +12,11 @@ export class NewContactDialogComponent implements OnInit {
   ngOnInit() {
   }
 
+  save() {
+
+  }
+
+  dismiss() {
+
+  }
 }
--- src/app/contactmanager/components/toolbar/toolbar.component.ts
+++ src/app/contactmanager/components/toolbar/toolbar.component.ts
@@ -18,8 +18,14 @@ export class ToolbarComponent implements OnInit {
   }
 
   openAddContactDialog(): void {
-    this.dialog.open(NewContactDialogComponent, {
+    const dialogRef = this.dialog.open(NewContactDialogComponent, {
       width: '450px'
     });
+
+    dialogRef.afterClosed().subscribe(
+      result => {
+        console.log('The dialog was closed', result);
+      }
+    );
   }
 }
