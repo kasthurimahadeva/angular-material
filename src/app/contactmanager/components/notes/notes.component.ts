0fd7f932931b1cac562cf3a7d814c099d5f50e0e
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-07-11 16:31:06 +0530

    mat-table

--- src/app/contactmanager/components/notes/notes.component.ts
+++ src/app/contactmanager/components/notes/notes.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Note} from '../../models/note';
+import {MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-notes',
@@ -9,10 +10,13 @@ import {Note} from '../../models/note';
 export class NotesComponent implements OnInit {
 
   @Input() notes: Note[];
+  displayedColumns = ['id', 'title', 'date'];
+  dataSource: MatTableDataSource<Note>;
 
   constructor() { }
 
   ngOnInit() {
+    this.dataSource = new MatTableDataSource<Note>(this.notes);
   }
 
 }
